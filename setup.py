31555f5664e0b01090667d053cb2dd3945026b1f
Carl Drougge <bearded@longhaired.org>
2013-01-16 12:04:34 +0100

    Link zlib (works without, but I assume depends on python linkage).

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 
-gzlinesmodule = Extension("gzlines", sources = ["gzlinesmodule.c"])
+gzlinesmodule = Extension("gzlines", sources = ["gzlinesmodule.c"], libraries=["z"])
 
 setup(name="gzlines", version="1.0", description="Read lines from gz files", ext_modules=[gzlinesmodule])
