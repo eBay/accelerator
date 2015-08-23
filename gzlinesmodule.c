a3e847aa93c3cad6b782c368dbf76944bad18648
Carl Drougge <bearded@longhaired.org>
2015-08-23 20:23:21 +0000

    +GzWriteLines (adds linefeed after each write).

--- gzlinesmodule.c
+++ gzlinesmodule.c
@@ -338,6 +338,17 @@ static PyObject *gzwrite_write_GzWrite(GzWrite *self, PyObject *args)
 	return gzwrite_write_(self, data, len);
 }
 
+static PyObject *gzwrite_write_GzWriteLines(GzWrite *self, PyObject *args)
+{
+	const char *data;
+	int len;
+	if (!PyArg_ParseTuple(args, "s#", &data, &len)) return 0;
+	PyObject *ret = gzwrite_write_(self, data, len);
+	if (!ret) return 0;
+	Py_DECREF(ret);
+	return gzwrite_write_(self, "\n", 1);
+}
+
 static PyObject *gzwrite_exit(PyObject *self, PyObject *args)
 {
 	PyObject *ret = PyObject_CallMethod(self, "close", NULL);
@@ -346,57 +357,60 @@ static PyObject *gzwrite_exit(PyObject *self, PyObject *args)
 	Py_RETURN_NONE;
 }
 
-static PyMethodDef GzWrite_methods[] = {
-	{"__enter__", (PyCFunction)gzwrite_self, METH_NOARGS,  NULL},
-	{"__exit__",  (PyCFunction)gzwrite_exit, METH_VARARGS, NULL},
-	{"write",     (PyCFunction)gzwrite_write_GzWrite, METH_VARARGS, NULL},
-	{"close",     (PyCFunction)gzwrite_close, METH_NOARGS,  NULL},
-	{NULL, NULL, 0, NULL}
-};
-
-static PyTypeObject GzWrite_Type = {
-	PyVarObject_HEAD_INIT(NULL, 0)
-	"GzWrite",                      /*tp_name*/
-	sizeof(GzWrite),                /*tp_basicsize*/
-	0,                              /*tp_itemsize*/
-	(destructor)gzwrite_dealloc,    /*tp_dealloc*/
-	0,                              /*tp_print*/
-	0,                              /*tp_getattr*/
-	0,                              /*tp_setattr*/
-	0,                              /*tp_compare*/
-	0,                              /*tp_repr*/
-	0,                              /*tp_as_number*/
-	0,                              /*tp_as_sequence*/
-	0,                              /*tp_as_mapping*/
-	0,                              /*tp_hash*/
-	0,                              /*tp_call*/
-	0,                              /*tp_str*/
-	0,                              /*tp_getattro*/
-	0,                              /*tp_setattro*/
-	0,                              /*tp_as_buffer*/
-	Py_TPFLAGS_DEFAULT,             /*tp_flags*/
-	0,                              /*tp_doc*/
-	0,                              /*tp_traverse*/
-	0,                              /*tp_clear*/
-	0,                              /*tp_richcompare*/
-	0,                              /*tp_weaklistoffset*/
-	0,                              /*tp_iter*/
-	0,                              /*tp_iternext*/
-	GzWrite_methods,                /*tp_methods*/
-	0,                              /*tp_members*/
-	0,                              /*tp_getset*/
-	0,                              /*tp_base*/
-	0,                              /*tp_dict*/
-	0,                              /*tp_descr_get*/
-	0,                              /*tp_descr_set*/
-	0,                              /*tp_dictoffset*/
-	gzwrite_init,                   /*tp_init*/
-	PyType_GenericAlloc,            /*tp_alloc*/
-	PyType_GenericNew,              /*tp_new*/
-	PyObject_Del,                   /*tp_free*/
-	0,                              /*tp_is_gc*/
-};
-
+#define MKWTYPE(name)                                                                  	\
+	static PyMethodDef name ## _methods[] = {                                      	\
+		{"__enter__", (PyCFunction)gzwrite_self, METH_NOARGS,  NULL},          	\
+		{"__exit__",  (PyCFunction)gzwrite_exit, METH_VARARGS, NULL},          	\
+		{"write",     (PyCFunction)gzwrite_write_ ## name, METH_VARARGS, NULL},	\
+		{"flush",     (PyCFunction)gzwrite_flush, METH_NOARGS, NULL},          	\
+		{"close",     (PyCFunction)gzwrite_close, METH_NOARGS, NULL},          	\
+		{NULL, NULL, 0, NULL}                                                  	\
+	};                                                                             	\
+	                                                                               	\
+	static PyTypeObject name ## _Type = {                        	\
+		PyVarObject_HEAD_INIT(NULL, 0)                       	\
+		#name,                          /*tp_name*/          	\
+		sizeof(GzWrite),                /*tp_basicsize*/     	\
+		0,                              /*tp_itemsize*/      	\
+		(destructor)gzwrite_dealloc,    /*tp_dealloc*/       	\
+		0,                              /*tp_print*/         	\
+		0,                              /*tp_getattr*/       	\
+		0,                              /*tp_setattr*/       	\
+		0,                              /*tp_compare*/       	\
+		0,                              /*tp_repr*/          	\
+		0,                              /*tp_as_number*/     	\
+		0,                              /*tp_as_sequence*/   	\
+		0,                              /*tp_as_mapping*/    	\
+		0,                              /*tp_hash*/          	\
+		0,                              /*tp_call*/          	\
+		0,                              /*tp_str*/           	\
+		0,                              /*tp_getattro*/      	\
+		0,                              /*tp_setattro*/      	\
+		0,                              /*tp_as_buffer*/     	\
+		Py_TPFLAGS_DEFAULT,             /*tp_flags*/         	\
+		0,                              /*tp_doc*/           	\
+		0,                              /*tp_traverse*/      	\
+		0,                              /*tp_clear*/         	\
+		0,                              /*tp_richcompare*/   	\
+		0,                              /*tp_weaklistoffset*/	\
+		0,                              /*tp_iter*/          	\
+		0,                              /*tp_iternext*/      	\
+		name ## _methods,               /*tp_methods*/       	\
+		0,                              /*tp_members*/       	\
+		0,                              /*tp_getset*/        	\
+		0,                              /*tp_base*/          	\
+		0,                              /*tp_dict*/          	\
+		0,                              /*tp_descr_get*/     	\
+		0,                              /*tp_descr_set*/     	\
+		0,                              /*tp_dictoffset*/    	\
+		gzwrite_init,                   /*tp_init*/          	\
+		PyType_GenericAlloc,            /*tp_alloc*/         	\
+		PyType_GenericNew,              /*tp_new*/           	\
+		PyObject_Del,                   /*tp_free*/          	\
+		0,                              /*tp_is_gc*/         	\
+	}
+MKWTYPE(GzWrite);
+MKWTYPE(GzWriteLines);
 
 
 #define INIT(name) do {                                              	\
@@ -422,6 +436,7 @@ PyMODINIT_FUNC initgzlines(void)
 	INIT(GzDate);
 	INIT(GzTime);
 	INIT(GzWrite);
+	INIT(GzWriteLines);
 	PyObject *version = Py_BuildValue("(iii)", 1, 3, 0);
 	PyModule_AddObject(m, "version", version);
 	// old name for compat
