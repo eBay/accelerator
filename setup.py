3eb213daa2f9342bb9e2ae9f2684496d6393636b
Carl Drougge <bearded@longhaired.org>
2015-10-28 16:54:50 +0000

    Fix building with GCC.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 
-gzutilmodule = Extension("gzutil", sources = ["gzutilmodule.c"], libraries=["z"])
+gzutilmodule = Extension("gzutil", sources = ["gzutilmodule.c"], libraries=["z"], extra_compile_args=['-std=c99'])
 
 setup(name="gzutil", version="2.0.1", description="Read/write values from/to gz files", ext_modules=[gzutilmodule])
