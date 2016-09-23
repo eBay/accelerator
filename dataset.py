0a5d3a4836880b0cfccff09f18e26355b5ff3749
Carl Drougge <bearded@longhaired.org>
2016-09-23 18:43:40 +0000

    Move the dataset chaining stuff to Dataset.
    This doesn't even preserve compat wrappers in the chaining module.

--- dataset.py
+++ dataset.py
@@ -9,13 +9,15 @@ from keyword import kwlist
 from collections import namedtuple
 from itertools import compress
 from functools import partial
+from inspect import getargspec
 
-from compat import unicode, uni
+from compat import unicode, uni, ifilter, imap, izip, str_types
 
 import blob
 from extras import DotDict
 from jobid import resolve_jobid_filename
 from gzwrite import typed_writer
+from status import status
 
 kwlist = set(kwlist)
 # Add some python3 keywords
@@ -255,16 +257,182 @@ class Dataset(unicode):
 		return chain
 
 	def iterate_chain(self, sliceno, columns=None, length=-1, reverse=False, hashlabel=None, stop_jobid=None, pre_callback=None, post_callback=None, filters=None, translators=None):
+		"""Iterate a list of datasets. See .chain and .iterate_list for details."""
 		chain = self.chain(length, reverse, stop_jobid)
 		return self.iterate_list(sliceno, columns, chain, hashlabel=hashlabel, pre_callback=pre_callback, post_callback=post_callback, filters=filters, translators=translators)
 
 	def iterate(self, sliceno, columns=None, hashlabel=None, filters=None, translators=None):
+		"""Iterate just this dataset. See .iterate_list for details."""
 		return self.iterate_list(sliceno, columns, [self], hashlabel=hashlabel, filters=filters, translators=translators)
 
 	@staticmethod
 	def iterate_list(sliceno, columns, jobids, hashlabel=None, pre_callback=None, post_callback=None, filters=None, translators=None):
-		from chaining import iterate_datasets
-		return iterate_datasets(sliceno, columns, jobids, hashlabel=hashlabel, pre_callback=pre_callback, post_callback=post_callback, filters=filters, translators=translators)
+		"""Iterator over the specified columns from jobids (str or list)
+		callbacks are called before and after each dataset is iterated.
+
+		filters decide which rows to include and can be a callable
+		(called with the candidate tuple), or a dict {name: filter}.
+		In the latter case each individual filter is called with the column
+		value, or if it's None uses the column value directly.
+		All filters must say yes to yield a row.
+		examples:
+		filters={'some_col': some_dict.get}
+		filters={'some_col': some_set.__contains__}
+		filters={'some_col': some_str.__eq__}
+		filters=lambda line: line[0] == line[1]
+
+		translators transform data values. It can be a callable (called with the
+		candidate tuple and expected to return a tuple of the same length) or a
+		dict {name: translation}.
+		Each translation can be a function (called with the column value and
+		returning the new value) or dict. Items missing in the dict yield None,
+		which can be removed with filters={'col': None}.
+
+		Translators run before filters.
+
+		You can also pass a single name (a str) as columns, in which case you
+		don't get a tuple back (just the values). Tuple-filters/translators also
+		get just the value in this case (column versions are unaffected).
+		
+		If you pass a false value for columns you get all columns in name order.
+		"""
+
+		if isinstance(jobids, Dataset):
+			jobids = [jobids]
+		if isinstance(jobids, str_types):
+			jobids = [jid.strip() for jid in jobids.split(',')]
+		if not columns:
+			columns = Dataset(jobids[0]).columns
+		if isinstance(columns, str_types):
+			columns = [columns]
+			want_tuple = False
+		else:
+			if isinstance(columns, dict):
+				columns = sorted(columns)
+			want_tuple = True
+		to_iter = []
+		if sliceno is None:
+			from g import SLICES
+		for jobid in jobids:
+			d = jobid if isinstance(jobid, Dataset) else Dataset(jobid)
+			jobid = jobid.split('/')[0]
+			if sliceno is None:
+				for ix in range(SLICES):
+					to_iter.append((jobid, d, ix, False,))
+			else:
+				if hashlabel and d.hashlabel != hashlabel:
+					assert hashlabel in d.columns, "Can't rehash %s on non-existant column %s" % (d, hashlabel,)
+					rehash = hashlabel
+				else:
+					rehash = False
+				to_iter.append((jobid, d, sliceno, rehash,))
+		filter_func = Dataset._resolve_filters(columns, filters)
+		translation_func, translators = Dataset._resolve_translators(columns, translators)
+		from itertools import chain
+		return chain.from_iterable(Dataset._iterate_datasets(to_iter, columns, pre_callback, post_callback, filter_func, translation_func, translators, want_tuple))
+
+	@staticmethod
+	def _resolve_filters(columns, filters):
+		if filters and not callable(filters):
+			# Sort in column order, to allow selecting an efficient order.
+			filters = sorted((columns.index(name), f,) for name, f in filters.items())
+			# Build "lambda t: f0(t[0]) and f1(t[1]) and ..."
+			fs = []
+			arg_n = []
+			arg_v = []
+			for ix, f in filters:
+				if f is None or f is bool:
+					# use value directly
+					fs.append('t[%d]' % (ix,))
+				else:
+					n = 'f%d' % (ix,)
+					arg_n.append(n)
+					arg_v.append(f)
+					fs.append('%s(t[%d])' % (n, ix,))
+			f = 'lambda t: ' + ' and '.join(fs)
+			# Add another lambda to put all fN into local variables.
+			# (This is faster than putting them in "locals", you get
+			# LOAD_DEREF instead of LOAD_GLOBAL.)
+			f = 'lambda %s: %s' % (', '.join(arg_n), f)
+			return eval(f, {}, {})(*arg_v)
+		else:
+			return filters
+
+	@staticmethod
+	def _resolve_translators(columns, translators):
+		if not translators:
+			return None, None
+		if callable(translators):
+			return translators, None
+		else:
+			res = {}
+			for name, f in translators.items():
+				if not callable(f):
+					f = f.get
+				res[columns.index(name)] = f
+			return None, res
+
+	@staticmethod
+	def _iterate_datasets(to_iter, columns, pre_callback, post_callback, filter_func, translation_func, translators, want_tuple):
+		skip_jobid = None
+		def argfixup(func, is_post):
+			if func:
+				if len(getargspec(func).args) == 1:
+					seen_jobid = [None]
+					def wrapper(jobid, sliceno=None):
+						if jobid != seen_jobid[0]:
+							if is_post:
+								if seen_jobid[0] and seen_jobid[0] != skip_jobid:
+									func(seen_jobid[0])
+							else:
+								func(jobid)
+							seen_jobid[0] = jobid
+					return wrapper, True
+			return func, False
+		pre_callback, unsliced_pre_callback = argfixup(pre_callback, False)
+		post_callback, unsliced_post_callback = argfixup(post_callback, True)
+		if not to_iter:
+			return
+		starting_at = '%s:%d' % (to_iter[0][0], to_iter[0][2],)
+		if len(to_iter) == 1:
+			msg = 'Iterating ' + starting_at
+		else:
+			msg = 'Iterating %d dataset slices starting at %s' % (len(to_iter), starting_at,)
+		with status(msg):
+			for ix, (jobid, d, sliceno, rehash) in enumerate(to_iter):
+				if unsliced_post_callback:
+					post_callback(jobid)
+				if pre_callback:
+					if jobid == skip_jobid:
+						continue
+					try:
+						pre_callback(jobid, sliceno)
+					except SkipSlice:
+						if unsliced_pre_callback:
+							skip_jobid = jobid
+						continue
+					except SkipJob:
+						skip_jobid = jobid
+						continue
+				it = d._iterator(None if rehash else sliceno, columns)
+				for ix, trans in (translators or {}).items():
+					it[ix] = imap(trans, it[ix])
+				if want_tuple:
+					it = izip(*it)
+				else:
+					it = it[0]
+				if rehash:
+					it = d._hashfilter(sliceno, rehash, it)
+				if translation_func:
+					it = imap(translation_func, it)
+				if filter_func:
+					it = ifilter(filter_func, it)
+				with status('(%d/%d) %s:%s' % (ix, len(to_iter), jobid, 'REHASH' if rehash else sliceno,)):
+					yield it
+				if post_callback and not unsliced_post_callback:
+					post_callback(jobid, sliceno)
+			if unsliced_post_callback:
+				post_callback(None)
 
 	@staticmethod
 	def new(columns, filenames, lines, minmax={}, filename=None, hashlabel=None, caption=None, previous=None, name='default'):
@@ -720,6 +888,15 @@ class DatasetWriter(object):
 			res = Dataset.new(**args)
 		del _datasetwriters[self.name]
 		return res
+
+class SkipJob(Exception):
+	"""Raise this in pre_callback to skip iterating the coming job
+	(or the remaining slices of it)"""
+
+class SkipSlice(Exception):
+	"""Raise this in pre_callback to skip iterating the coming slice
+	(if your callback doesn't want sliceno, this is the same as SkipJob)"""
+
 
 # Backward compat sadness.
 # Supports some of the info API, none of the reading or writing API.
