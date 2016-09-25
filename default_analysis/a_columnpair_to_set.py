0308800298ea0993b0ec2faae693516c5eb27c04
Carl Drougge <bearded@longhaired.org>
2016-09-25 19:48:19 +0000

    [columnpair_to_set] Use the modern API.

--- default_analysis/a_columnpair_to_set.py
+++ default_analysis/a_columnpair_to_set.py
@@ -2,8 +2,8 @@ from __future__ import division
 
 from collections import defaultdict, Counter
 
-from chaining import jobchain_prev, iterate_datasetchain
-from extras import pickle_load, pickle_save, job_params, JobWithFile, OptionString
+from extras import job_params, JobWithFile, OptionString
+import blob
 
 options = {
 	'key_filter'  : JobWithFile, # a set of keys to keep (or nothing)
@@ -16,20 +16,18 @@ jobids = ('previous',)
 
 
 def prepare(jobids):
-	key_filter   = pickle_load(options.key_filter, default=set()),
-	value_filter = pickle_load(options.value_filter, default=set())
+	key_filter   = blob.load(options.key_filter, default=set()),
+	value_filter = blob.load(options.value_filter, default=set())
 	return key_filter, value_filter
 
 def analysis(sliceno, prepare_res):
 	key_filter, value_filter = prepare_res
-	prev = jobchain_prev()
-	d = pickle_load(jobid=prev, sliceno=sliceno, default=defaultdict(set))
+	d = blob.load(jobid=jobids.previous, sliceno=sliceno, default=defaultdict(set))
 	if options.key_filter:
 		d = {k: v for k, v in d.iteritems() if k in key_filter}
-	iterator = iterate_datasetchain(
+	iterator = datasets.src.iterate_chain(
 		sliceno,
 		(options.key_column, options.value_column,),
-		tip_jobid=datasets.src,
 		stop_jobid=job_params(jobids.previous, default_empty=True).datasets.src,
 	)
 	# These break out into four versions for shorter runtime
@@ -60,12 +58,12 @@ def analysis(sliceno, prepare_res):
 		else:
 			for k, v in iterator:
 				d[k].add(reuse.setdefault(k, k))
-	pickle_save(d, sliceno=sliceno)
-	pickle_save(set(d), 'keyset', sliceno=sliceno)
-	pickle_save(Counter(len(v) for v in d.itervalues()), 'setsizehist', sliceno=sliceno)
+	blob.save(d, sliceno=sliceno, temp=False)
+	blob.save(set(d), 'keyset', sliceno=sliceno, temp=False)
+	blob.save(Counter(len(v) for v in d.itervalues()), 'setsizehist', sliceno=sliceno, temp=False)
 
 def synthesis(params):
 	setsizehist = Counter()
 	for sliceno in range(params.slices):
-		setsizehist.update(pickle_load('setsizehist', sliceno=sliceno))
-	pickle_save(setsizehist, 'setsizehist')
+		setsizehist.update(blob.load('setsizehist', sliceno=sliceno))
+	blob.save(setsizehist, 'setsizehist')
