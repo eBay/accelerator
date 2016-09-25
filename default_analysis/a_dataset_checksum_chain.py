6c2f84d205b19ca243243e0d99b238a4d9b8f702
Carl Drougge <bearded@longhaired.org>
2016-09-25 18:44:43 +0000

    [dataset_checksum_chain] Use the modern API.

--- default_analysis/a_dataset_checksum_chain.py
+++ default_analysis/a_dataset_checksum_chain.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from __future__ import print_function
 
 description = r'''
 Take a chain of datasets and make a checksum of one or more columns.
@@ -10,9 +11,9 @@ options.chain_length defaults to -1.
 Sort does not sort across datasets.
 '''
 
-from chaining import jobchain
 from subjobs import build
-from extras import DotDict, pickle_load
+from extras import DotDict
+import blob
 
 options = dict(
 	chain_length = -1,
@@ -24,10 +25,10 @@ datasets = ('source', 'stop',)
 
 def synthesis():
 	sum = 0
-	jobs = jobchain(length=options.chain_length, tip_jobid=datasets.source, stop_jobid=datasets.stop)
+	jobs = datasets.source.chain(length=options.chain_length, stop_jobid=datasets.stop)
 	for src in jobs:
 		jid = build('dataset_checksum', options=dict(columns=options.columns, sort=options.sort), datasets=dict(source=src))
-		data = pickle_load(jobid=jid)
+		data = blob.load(jobid=jid)
 		sum ^= data.sum
-	print "Total: %016x" % (sum,)
+	print("Total: %016x" % (sum,))
 	return DotDict(sum=sum, columns=data.columns, sort=options.sort, sources=jobs)
