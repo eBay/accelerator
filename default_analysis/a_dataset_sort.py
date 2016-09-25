973371f10168b3c512470eae09429fea7547f981
Carl Drougge <bearded@longhaired.org>
2016-09-25 20:24:25 +0000

    [dataset_sort] Use the modern API.
    This even used the old dataset writing API, which didn't work any more.

--- default_analysis/a_dataset_sort.py
+++ default_analysis/a_dataset_sort.py
@@ -5,13 +5,11 @@ from __future__ import division
 
 from numpy import lexsort
 from os import symlink
+from functools import partial
 
-from gzwrite import typed_writer
-from extras import job_params, mk_splitdir, OptionEnum, OptionString
+from extras import job_params, OptionEnum, OptionString
 from jobid import resolve_jobid_filename
-from chaining import jobchain, iterate_datasets
-from dataset import dataset
-import blob
+from dataset import Dataset, DatasetWriter
 
 OrderEnum = OptionEnum('ascending descending')
 
@@ -23,9 +21,9 @@ options = {
 datasets = ('source', 'previous',)
 
 
-def sort(columniter, columns):
+def sort(columniter):
 	def sortable_columnlist(column):
-		if columns[column] in ('datetime', 'date', 'time',):
+		if datasets.source.columns[column] in ('datetime', 'date', 'time',):
 			return map(str, columniter(column))
 		else:
 			return list(columniter(column))
@@ -39,68 +37,51 @@ def sort(columniter, columns):
 		sort_idx = list(lexsort(lst)) # stable
 	return sort_idx
 
-def prepare():
+def prepare(params):
 	prev_p = job_params(datasets.previous, default_empty=True)
-	d = dataset()
-	d.load(datasets.source)
-	columns = d.name_type_dict()
-	jobs = jobchain(tip_jobid=datasets.source, stop_jobid=prev_p.datasets.source)
+	d = datasets.source
+	jobs = d.chain(stop_jobid=prev_p.datasets.source)
 	if options.sort_across_slices:
-		def columniter(column):
-			return iterate_datasets(None, column, jobs)
-		sort_idx = sort(columniter, columns)
+		columniter = partial(Dataset.iterate_list, None, jobids=jobs)
+		sort_idx = sort(columniter)
 	else:
 		sort_idx = None
-	return columns, jobs, sort_idx
+	if options.sort_across_slices:
+		hashlabel = None
+	else:
+		hashlabel = d.hashlabel
+	if len(jobs) == 1:
+		filename = d.filename
+	else:
+		filename = None
+	dw = DatasetWriter(
+		columns=d.columns,
+		caption=params.caption,
+		hashlabel=hashlabel,
+		filename=filename,
+	)
+	return dw, jobs, sort_idx
 
 def analysis(sliceno, params, prepare_res):
-	columns, jobs, sort_idx = prepare_res
+	dw, jobs, sort_idx = prepare_res
 	single_job = (len(jobs) == 1)
 	if options.sort_across_slices:
-		def columniter(column):
-			return iterate_datasets(None, column, jobs)
+		columniter = partial(Dataset.iterate_list, None, jobids=jobs)
 		per_slice = len(sort_idx) // params.slices
 		if sliceno + 1 ==  params.slices:
 			sort_idx = sort_idx[per_slice * sliceno:]
 		else:
 			sort_idx = sort_idx[per_slice * sliceno:per_slice * (sliceno + 1)]
 	else:
-		def columniter(column):
-			return iterate_datasets(sliceno, column, jobs)
-		sort_idx = sort(columniter, columns)
-	if single_job and sort_idx == sorted(sort_idx) and not options.sort_across_slices:
+		columniter = partial(Dataset.iterate_list, sliceno, jobids=jobs)
+		sort_idx = sort(columniter)
+	if single_job and not options.sort_across_slices and sort_idx == sorted(sort_idx):
 		# this slice is fully sorted as is.
 		slice_dir = '%02d' % (sliceno,)
 		symlink(resolve_jobid_filename(datasets.source, slice_dir), slice_dir)
 		return len(sort_idx)
-	dstdir = mk_splitdir(sliceno)
-	def writer(column):
-		W = typed_writer(columns[column])
-		return W('%s/%s.gz' % (dstdir, column,))
-	for column in columns:
+	for column in datasets.source.columns:
 		lst = list(columniter(column))
-		with writer(column) as w:
-			for idx in sort_idx:
-				w.write(lst[idx])
-	return len(sort_idx)
-
-def synthesis(params, analysis_res):
-	d = dataset()
-	d.load(datasets.source)
-	if options.sort_across_slices:
-		hashlabel = None
-	else:
-		hashlabel = d.get_hashlabel()
-	dataset().new(params.jobid,
-		name_type=d.name_type_dict(),
-		caption=params.caption,
-		num_lines_per_split=list(analysis_res),
-		hashlabel=hashlabel,
-		filename=d.get_filename(),
-	)
-	# Preserve minmax.pickle if source has it.
-	try:
-		minmax = blob.load('minmax', datasets.source)
-		blob.save(minmax, 'minmax')
-	except IOError:
-		pass
+		w = dw.writers[column].write
+		for idx in sort_idx:
+			w(lst[idx])
