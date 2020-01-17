1ac8134959a22b8601efe14d45793f89acb26efe
Carl Drougge <bearded@longhaired.org>
2020-01-17 10:00:33 +0000

    [dsinfo] +non_empty_chain

--- accelerator/dsinfo.py
+++ accelerator/dsinfo.py
@@ -3,7 +3,7 @@
 ############################################################################
 #                                                                          #
 # Copyright (c) 2017 eBay Inc.                                             #
-# Modifications copyright (c) 2019 Carl Drougge                            #
+# Modifications copyright (c) 2019-2020 Carl Drougge                       #
 # Modifications copyright (c) 2019-2020 Anders Berkeman                    #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
@@ -61,6 +61,7 @@ def main(argv):
 	usage = "%(prog)s [options] ds [ds [...]]"
 	parser = argparse.ArgumentParser(prog=argv.pop(0), usage=usage)
 	parser.add_argument('-c', '--chain', action='store_true', help='list all datasets in a chain')
+	parser.add_argument('-C', '--non_empty_chain', action='store_true', help='list all non-empty datasets in a chain')
 	parser.add_argument('-l', '--list', action='store_true', help='list all datasets in a job')
 	parser.add_argument('-L', '--chainedlist', action='store_true', help='list all datasets in a job, @@@@@@@@@@@@@@')
 	parser.add_argument('-m', '--suppress_minmax', action='store_true', help='do not print min/max column values')
@@ -70,6 +71,7 @@ def main(argv):
 	parser.add_argument('-S', '--chainedslices', action='store_true', help='same as -s but for full chain')
 	parser.add_argument("dataset", nargs='+')
 	args = parser.parse_args(argv)
+	args.chain = args.chain or args.non_empty_chain
 
 	def finish(badinput):
 		if badinput and not args.suppress_errors:
@@ -163,12 +165,18 @@ def main(argv):
 
 		if ds.previous or args.chain:
 			chain = ds.chain()
-			print("    Chain length {0:n}, from {1} to {2}".format(len(chain), chain[0], chain[-1]))
-			if args.chain:
-				data = tuple((ix, "%s/%s" % (x.job, x.name), "{:n}".format(sum(x.lines))) for ix, x in enumerate(chain))
-				max_n, max_l = colwidth(x[1:] for x in data)
-				template = "{0:3}: {1:%d} ({2:>%d})" % (max_n, max_l)
-				printcolwise(data, template, lambda x: (x[0], x[1], x[2]), minrows=8, indent=8)
+			if args.non_empty_chain:
+				print("    Full chain length {0:n}, from {1} to {2}".format(len(chain), chain[0], chain[-1]))
+				chain = [ds for ds in chain if sum(ds.lines)]
+				print("    Filtered chain length {0:n}".format(len(chain)))
+			if chain:
+				if not args.non_empty_chain:
+					print("    Chain length {0:n}, from {1} to {2}".format(len(chain), chain[0], chain[-1]))
+				if args.chain:
+					data = tuple((ix, "%s/%s" % (x.job, x.name), "{:n}".format(sum(x.lines))) for ix, x in enumerate(chain))
+					max_n, max_l = colwidth(x[1:] for x in data)
+					template = "{0:3}: {1:%d} ({2:>%d})" % (max_n, max_l)
+					printcolwise(data, template, lambda x: (x[0], x[1], x[2]), minrows=8, indent=8)
 
 		if args.slices or args.chainedslices:
 			if args.chainedslices and ds.previous:
