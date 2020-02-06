734a9aa2bd39365f58f834b778064b43d0aa89fc
Anders Berkeman <anders@berkeman.org>
2020-02-06 21:35:48 +0100

    [job] +job.files()

--- accelerator/job.py
+++ accelerator/job.py
@@ -103,6 +103,10 @@ class Job(unicode):
 		assert 'r' in mode, "Don't write to other jobs"
 		return open(self.filename(filename, sliceno), mode, encoding=encoding, errors=errors)
 
+	def files(self, pattern=''):
+		from fnmatch import filter
+		return set(filter(self.post.files, pattern))
+
 	def withfile(self, filename, sliced=False, extra=None):
 		return JobWithFile(self, filename, sliced, extra)
 
