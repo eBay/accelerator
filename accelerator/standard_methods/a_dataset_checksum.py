a6e2298fe86e275d212590b246a64f7bb33c057b
Carl Drougge <bearded@longhaired.org>
2020-05-07 19:55:54 +0000

    [dataset_checksum] Sort dicts so json cols work.
    Since I'm messing with it anyway, change how string types are hashed
    just to make it faster.

--- accelerator/standard_methods/a_dataset_checksum.py
+++ accelerator/standard_methods/a_dataset_checksum.py
@@ -1,7 +1,7 @@
 ############################################################################
 #                                                                          #
 # Copyright (c) 2017 eBay Inc.                                             #
-# Modifications copyright (c) 2018-2019 Carl Drougge                       #
+# Modifications copyright (c) 2018-2020 Carl Drougge                       #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
@@ -38,6 +38,7 @@ datasets. (So one GB per 20M lines or so.)
 
 from hashlib import md5
 from itertools import chain
+from json import JSONEncoder
 
 from accelerator.extras import DotDict
 from accelerator.compat import PY2
@@ -49,18 +50,65 @@ options = dict(
 
 datasets = ('source',)
 
+if PY2:
+	bytesrepr = repr
+else:
+	def bytesrepr(v):
+		return repr(v).encode('utf-8')
+
+def bytesstr(v):
+	return v.encode('utf-8')
+
+def bytesstr_none(v):
+	return b'None\xff' if v is None else v.encode('utf-8')
+
+def self_none(v):
+	return b'None\xff' if v is None else v
+
+jsonenc = JSONEncoder(
+	sort_keys=True,
+	ensure_ascii=False,
+	check_circular=False,
+	separators=(',', ':'),
+).encode
+def sortdicts(v):
+	return jsonenc(v).encode('utf-8')
+
 def prepare():
-	return sorted(options.columns or datasets.source.columns)
+	columns = sorted(options.columns or datasets.source.columns)
+	translators = {}
+	# Special casing the string types here is just a performance
+	# optimisation. (Which changes the hash, but not how good it is.)
+	# Special casing json is needed because the iteration order of
+	# dicts is not fixed (depending on python version).
+	for n in columns:
+		col = datasets.source.columns[n]
+		if col.type == 'bytes' or (col.type == 'ascii' and PY2):
+			# doesn't need any encoding, but might need None-handling.
+			if col.none_support:
+				translators[n] = self_none
+		elif col.type in ('unicode', 'ascii'):
+			if col.none_support:
+				translators[n] = bytesstr_none
+			else:
+				translators[n] = bytesstr
+		elif col.type == 'json':
+			translators[n] = sortdicts
+		else:
+			translators[n] = bytesrepr
+	return columns, translators
 
 def analysis(sliceno, prepare_res):
-	columns = prepare_res
-	if len(columns) == 1:
-		columns = columns[0]
-	src = datasets.source.iterate(sliceno, columns)
-	if PY2:
-		return [md5(repr(line)).digest() for line in src]
-	else:
-		return [md5(repr(line).encode("utf-8")).digest() for line in src]
+	columns, translators = prepare_res
+	src = datasets.source.iterate(sliceno, columns, translators=translators)
+	res = []
+	for line in src:
+		m = md5()
+		for item in line:
+			m.update(item)
+			m.update(b'\0') # Some separator is better than nothing.
+		res.append(m.digest())
+	return res
 
 def synthesis(prepare_res, analysis_res):
 	all = chain.from_iterable(analysis_res)
