3ab8b366e54788ecac112528f990f03e664e0270
Carl Drougge <bearded@longhaired.org>
2020-09-21 21:13:22 +0000

    [config] All paths should be relative to project directory.
    Since the config can set the project directory this means going over it
    twice. Most of this diff is just moving code to make that work.
    
    While we're at it let's call realpath() on everything too.

--- accelerator/configfile.py
+++ accelerator/configfile.py
@@ -61,7 +61,6 @@ def fixup_listen(project_directory, listen, urd=False):
 def load_config(filename):
 	from accelerator.error import UserError
 
-	key = None
 	multivalued = {'workdirs', 'method packages', 'interpreters'}
 	required = {'slices', 'workdirs', 'method packages'}
 	known = {'target workdir', 'listen', 'urd', 'board listen', 'result directory', 'input directory', 'project directory'} | required | multivalued
@@ -70,7 +69,7 @@ def load_config(filename):
 
 	def fixpath(fn):
 		# convert relative path to absolute wrt location of config file
-		return os.path.join(os.path.dirname(filename), fn)
+		return os.path.realpath(os.path.join(project_directory, fn))
 
 	class _E(Exception):
 		pass
@@ -106,35 +105,51 @@ def load_config(filename):
 		workdirs=check_workdirs,
 	)
 
+	with open(filename, 'r', encoding='utf-8') as fh:
+		lines = list(enumerate(fh, 1))
+	def parse(handle):
+		key = None
+		for n, line in lines:
+			lineno[0] = n
+			line = line.split('#', 1)[0].rstrip()
+			if not line.strip():
+				continue
+			if line == line.strip():
+				if ':' not in line:
+					raise _E('Expected a ":"')
+				key, val = line.split(':', 1)
+				if key not in known:
+					raise _E('Unknown key %r' % (key,))
+			else:
+				if not key:
+					raise _E('First line indented')
+				val = line
+			val = interpolate(val).strip()
+			if val:
+				handle(key, val)
+	def just_project_directory(key, val):
+		if key == 'project directory':
+			project_directory[0] = val
+	def everything(key, val):
+		if key in parsers:
+			val = parsers[key](val)
+		if key in checkers:
+			checkers[key](val)
+		if key in multivalued:
+			cfg[key].append(val)
+		else:
+			if key in cfg:
+				raise _E("%r doesn't take multiple values" % (key,))
+			cfg[key] = val
+
 	try:
-		with open(filename, 'r', encoding='utf-8') as fh:
-			for lineno, line in enumerate(fh, 1):
-				line = line.split('#', 1)[0].rstrip()
-				if not line.strip():
-					continue
-				if line == line.strip():
-					if ':' not in line:
-						raise _E('Expected a ":"')
-					key, val = line.split(':', 1)
-					if key not in known:
-						raise _E('Unknown key %r' % (key,))
-				else:
-					if not key:
-						raise _E('First line indented')
-					val = line
-				val = interpolate(val).strip()
-				if val:
-					if key in parsers:
-						val = parsers[key](val)
-					if key in checkers:
-						checkers[key](val)
-					if key in multivalued:
-						cfg[key].append(val)
-					else:
-						if key in cfg:
-							raise _E("%r doesn't take multiple values" % (key,))
-						cfg[key] = val
-		lineno = None
+		project_directory = [os.path.dirname(filename)]
+		lineno = [None]
+		parse(just_project_directory)
+		lineno = [None]
+		project_directory = os.path.realpath(project_directory[0])
+		parse(everything)
+		lineno = [None]
 
 		missing = set()
 		for req in required:
@@ -157,7 +172,7 @@ def load_config(filename):
 			res.target_workdir = res.workdirs[0][0]
 		if 'project_directory' not in res:
 			res.project_directory = os.path.dirname(filename)
-		res.project_directory = os.path.normpath(res.project_directory)
+		res.project_directory = os.path.realpath(res.project_directory)
 		res.workdirs = dict(res.workdirs)
 		if res.target_workdir not in res.workdirs:
 			raise _E('target workdir %r not in defined workdirs %r' % (res.target_workdir, set(res.workdirs),))
@@ -169,10 +184,10 @@ def load_config(filename):
 			res.urd_listen, res.urd = None, None
 		res.board_listen, _ = fixup_listen(res.project_directory, res.get('board_listen', ('.socket.dir/board', None)))
 	except _E as e:
-		if lineno is None:
+		if lineno[0] is None:
 			prefix = 'Error in %s:\n' % (filename,)
 		else:
-			prefix = 'Error on line %d of %s:\n' % (lineno, filename,)
+			prefix = 'Error on line %d of %s:\n' % (lineno[0], filename,)
 		raise UserError(prefix + e.args[0])
 
 	return res
