1e3d2f09192c6f40fca7979b31497a5d396d8589
Carl Drougge <bearded@longhaired.org>
2020-09-24 18:04:46 +0000

    [build_wheels] Switch paths around.

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This is for running in a manylinux2010 docker image, so /bin/bash is fine.
-# docker run --rm -v /tmp/io:/io quay.io/pypa/manylinux2010_x86_64 /io/accelerator/scripts/build_wheels.sh 20xx.xx.xx.dev1
+# docker run --rm -v /some/where:/out -v /path/to/accelerator:/accelerator --tmpfs /tmp quay.io/pypa/manylinux2010_x86_64 /accelerator/scripts/build_wheels.sh 20xx.xx.xx.dev1
 
 set -euo pipefail
 
@@ -11,7 +11,8 @@ fi
 
 set -x
 
-test -d /io/wheelhouse || exit 1
+test -d /out/wheelhouse || exit 1
+test -d /accelerator/accelerator || exit 1
 
 case "$1" in
 	20[2-9][0-9].[01][0-9].[0-3][0-9])
@@ -26,14 +27,14 @@ esac
 VERSION="$1"
 NAME="accelerator-$(echo "$1" | sed s/\\.0/./g)"
 
-cd /io/accelerator
+cd /accelerator
 ACCELERATOR_BUILD_VERSION="$VERSION" /opt/python/cp38-cp38/bin/python3 ./setup.py sdist
 cd /
 
 
 MANYLINUX_VERSION="${AUDITWHEEL_PLAT/%_*}"
 AUDITWHEEL_ARCH="${AUDITWHEEL_PLAT/${MANYLINUX_VERSION}_}"
-ZLIB_PREFIX="/io/zlib-ng-$AUDITWHEEL_PLAT"
+ZLIB_PREFIX="/out/zlib-ng-$AUDITWHEEL_PLAT"
 # we are (probably) in a 2010 container, but the wheel will be 1-compatible.
 AUDITWHEEL_PLAT="manylinux1_$AUDITWHEEL_ARCH"
 
@@ -60,12 +61,12 @@ for V in $(ls /opt/python/); do
 	case "$V" in
 		cp27-*|cp3[5-9]-*)
 			UNFIXED_NAME="unfixed_wheels/$NAME-$V-linux_$AUDITWHEEL_ARCH.whl"
-			FIXED_NAME="/io/wheelhouse/$NAME-$V-$AUDITWHEEL_PLAT.whl"
+			FIXED_NAME="/out/wheelhouse/$NAME-$V-$AUDITWHEEL_PLAT.whl"
 			rm -f "$UNFIXED_NAME" "$FIXED_NAME"
 			ACCELERATOR_BUILD_STATIC_ZLIB="$ZLIB_PREFIX/lib/libz.a" \
 			CPPFLAGS="-I$ZLIB_PREFIX/include" \
-			"/opt/python/$V/bin/pip" wheel /io/accelerator/dist/"$NAME".tar.gz --no-deps -w unfixed_wheels/
-			auditwheel repair "$UNFIXED_NAME" -w /io/wheelhouse/
+			"/opt/python/$V/bin/pip" wheel /accelerator/dist/"$NAME".tar.gz --no-deps -w unfixed_wheels/
+			auditwheel repair "$UNFIXED_NAME" -w /out/wheelhouse/
 			"/opt/python/$V/bin/pip" install "$FIXED_NAME"
 			rm -rf /tmp/axtest
 			"/opt/python/$V/bin/ax" init --slices "$SLICES" --name "${V/*-/}" /tmp/axtest
