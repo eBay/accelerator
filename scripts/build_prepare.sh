83030ca9b4f3a231b8993a9edafc97f943509891
Carl Drougge <bearded@longhaired.org>
2021-12-27 08:39:16 +0100

    [buildscripts] update zlib-ng to 2.0.6

--- scripts/build_prepare.sh
+++ scripts/build_prepare.sh
@@ -47,7 +47,7 @@ cd /tmp
 rm -rf zlib-ng
 git clone https://github.com/zlib-ng/zlib-ng.git
 cd zlib-ng
-git checkout c69f78bc5e18a0f6de2dcbd8af863f59a14194f0 # 2.0.5
+git checkout b56a2fd0b126cfe5f13e68ab9090cd4f6a773286 # 2.0.6
 CFLAGS="-fPIC -fvisibility=hidden" ./configure --zlib-compat --static --prefix="$ZLIB_PREFIX"
 make install
 cd ..
